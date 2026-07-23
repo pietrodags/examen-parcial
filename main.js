8b85604828f031f01675b5b4b70fb66c5c8b49a2
pietrodags <289675776+pietrodags@users.noreply.github.com>
2026-07-23 11:32:10 +0200

    chore: create entire logic in main.js before SoC

--- main.js
+++ main.js
@@ -3,4 +3,38 @@
 
 
 const inputCode = document.querySelector("#code-input")
+const button = document.querySelector("#check")
 const output = document.querySelector("#output-area")
+const error = code => `El código ${code} no tiene el formato correcto`
+const success = code => `Se ha introducido el código ${code} en la base de datos`
+
+const codeList = []
+
+function validateCode(code) {
+    const codeFormat = code
+    const regex = /^[A-Z]{3}-\d{4}$/
+    return regex.test(codeFormat)
+
+}
+
+function showResult() {
+    const codeToCheck = inputCode.value.trim()
+    let message
+
+    if (validateCode(codeToCheck)) {
+        codeList.push(codeToCheck)
+        message = success(codeToCheck)
+    } else {
+        message = error(codeToCheck)
+    }
+
+    output.textContent = message
+
+
+
+
+    
+
+}
+
+button.addEventListener("click", showResult)
